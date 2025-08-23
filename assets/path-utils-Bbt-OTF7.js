782bdad2174767555cb5876bc736dc88afaa9ba3
nuforge <nuforge@users.noreply.github.com>
2025-08-23 21:36:52 +0000

    deploy: 654fad0b444ffa15dcfbd78b9e2063f260e5d0a6

--- assets/path-utils-Bbt-OTF7.js
+++
@@ -1,1 +0,0 @@
-const n=()=>typeof window<"u"?"/clca-courier":"",r=t=>{const e=n(),a=t.startsWith("/")?t.slice(1):t;return`${e}/${a}`},s=t=>r(`data/${t}`);export{s as a,r as g};
--- assets/use-quasar-BHZBCShy.js
+++
@@ -1,1 +0,0 @@
-import{a,bs as r}from"./index-B4l13YHj.js";function u(){return a(r)}export{u};
